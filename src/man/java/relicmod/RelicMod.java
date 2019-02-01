36b2661d4a518a7a135385c44a54b0a69e827dea
Anders <Anders@LAPTOP-33JGM1KD>
2019-02-01 13:06:12 +0100

    Added logic for relics.
    Begun on first relic "Black Lotus".

--- src/man/java/relicmod/RelicMod.java
+++ src/man/java/relicmod/RelicMod.java
@@ -1,17 +1,26 @@
 package relicmod;
 
+import java.nio.charset.StandardCharsets;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.localization.RelicStrings;
 
 import basemod.BaseMod;
 import basemod.interfaces.EditRelicsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
 
 @SpireInitializer
-public class RelicMod implements EditRelicsSubscriber {
-
+public class RelicMod implements EditRelicsSubscriber, EditStringsSubscriber {
+	
 	public static final Logger logger = LogManager.getLogger(RelicMod.class.getName());
+	
+	
+	private static String language = "english";
 	
 	public RelicMod() {
 		logger.info("RelicMod was created.");
@@ -26,6 +35,23 @@ public class RelicMod implements EditRelicsSubscriber {
 	public void receiveEditRelics() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void receiveEditStrings() {
+		final String pathToRelicStrings = "localization/" + RelicMod.language + "/RelicString.json";
+		String relicStrings = Gdx.files.internal(pathToRelicStrings).readString(String.valueOf(StandardCharsets.UTF_8));
+		
+		BaseMod.loadCustomStrings(RelicStrings.class, relicStrings);
+	}
+	
+	
+	public static final Texture getTexture(String resource) {
+		return new Texture("images/" + resource);
+	}
+	
+	public static final Texture getRelicTexture(String relic) {
+		return getTexture("relics/" + relic);
 	}
 	
 }
