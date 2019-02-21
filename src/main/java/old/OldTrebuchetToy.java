369cf50a18a8bb1f58d11500a286e03be920736e
Anders <Anders@LAPTOP-33JGM1KD>
2019-02-21 14:31:46 +0100

    Reworked Trebuchet Toy relic!
    Now deals more damage to all enemies, but only when potion is thrown.
    Solves issue #15.

--- src/main/java/old/OldTrebuchetToy.java
+++ src/main/java/old/OldTrebuchetToy.java
@@ -1,8 +1,5 @@
 package old;
 
-import java.util.HashMap;
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,7 +8,6 @@ import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 
 import relicmod.RelicMod;
@@ -23,83 +19,26 @@ public class OldTrebuchetToy extends AbstractRelicModRelic {
 	
 	public static final String ID = "Trebuchet_Toy";
 	
-	private static final int POTIONTHROWNEXTRADAMAGE = 10;
-	
-	
-	private HashMap<AbstractPotion, Integer> preUsePotionsMap;
+	private static final int POTIONTHROWNEXTRADAMAGE = 8;
 	
 	
 	public OldTrebuchetToy() {
-		super(ID, RelicTier.SHOP, LandingSound.FLAT);
-		 //this.preUsePotionsMap = createPotionCount(AbstractDungeon.player.potions);
+		super(ID, RelicTier.SHOP, LandingSound.HEAVY);
 	}
 	
-	
-	@Override
-	public void atTurnStart() {
-		this.preUsePotionsMap = createPotionCount(AbstractDungeon.player.potions);
-	}
 	
 	@Override
 	public void onUsePotion() {
-		AbstractPotion lastUsedPotion = null;
+		this.flash();
+		AbstractDungeon.actionManager.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
 		
-		HashMap<AbstractPotion, Integer> potionsRightNow = createPotionCount(AbstractDungeon.player.potions);
-		
-		boolean potionFound = false;
-		for (AbstractPotion potion : preUsePotionsMap.keySet()) {
-			if (!potionsRightNow.containsKey(potion)) {
-				potionFound = true;
-				
-			} else if (potionsRightNow.get(potion) < preUsePotionsMap.get(potion)) {
-				potionFound = true;
-			}
-			
-			if (potionFound) {
-				lastUsedPotion = potion;
-				break;
-			}
-			
-		}
-		
-		logger.info("Potion found!");
-		if (lastUsedPotion!=null) {
-			logger.info("Potion is " + lastUsedPotion.ID);
-			if (lastUsedPotion.isThrown) {
-				// Relic finally activates
-				this.flash();
-				AbstractDungeon.actionManager.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-				
-				AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(POTIONTHROWNEXTRADAMAGE, true), DamageType.NORMAL, AbstractGameAction.AttackEffect.SMASH));
-			}
-			
-		} else {
-			logger.info("Warning: Could not find last potion used?");
-		}
-		
-		this.preUsePotionsMap = createPotionCount(AbstractDungeon.player.potions);
+		AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(POTIONTHROWNEXTRADAMAGE, true), DamageType.NORMAL, AbstractGameAction.AttackEffect.SMASH));
 	}
 	
 	
 	@Override
 	public String getUpdatedDescription() {
 		return DESCRIPTIONS[0] + POTIONTHROWNEXTRADAMAGE + DESCRIPTIONS[1];
-	}
-	
-	
-	private static HashMap<AbstractPotion, Integer> createPotionCount(List<AbstractPotion> list) {
-		HashMap<AbstractPotion, Integer> map = new HashMap<AbstractPotion, Integer>();
-		
-		for (AbstractPotion potion : list) {
-			if (map.containsKey(potion)) {
-				map.put(potion, map.get(potion) + 1);
-				
-			} else {
-				map.put(potion, 1);
-			}
-		}
-		
-		return map;
 	}
 	
 }
--- src/main/java/relics/TrebuchetToy.java
+++ src/main/java/relics/TrebuchetToy.java
@@ -8,37 +8,47 @@ import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.evacipated.cardcrawl.mod.stslib.relics.BetterOnUsePotionRelic;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 
 import relicmod.RelicMod;
 import relics.AbstractRelicModRelic;
 
-public class TrebuchetToy extends AbstractRelicModRelic {
+public class TrebuchetToy extends AbstractRelicModRelic implements BetterOnUsePotionRelic {
 
 	public static final Logger logger = LogManager.getLogger(RelicMod.class.getName());
 	
 	public static final String ID = "Trebuchet_Toy";
 	
-	private static final int POTIONTHROWNEXTRADAMAGE = 8;
+	private static final int POTIONTHROWNEXTRADAMAGE = 10;
 	
 	
 	public TrebuchetToy() {
 		super(ID, RelicTier.SHOP, LandingSound.HEAVY);
-	}
-	
-	
-	@Override
-	public void onUsePotion() {
-		this.flash();
-		AbstractDungeon.actionManager.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
 		
-		AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(POTIONTHROWNEXTRADAMAGE, true), DamageType.NORMAL, AbstractGameAction.AttackEffect.SMASH));
 	}
+	
 	
 	
 	@Override
 	public String getUpdatedDescription() {
 		return DESCRIPTIONS[0] + POTIONTHROWNEXTRADAMAGE + DESCRIPTIONS[1];
+	}
+
+
+
+	@Override
+	public void betterOnUsePotion(AbstractPotion arg0) {
+		if (arg0.isThrown) {
+			this.flash();
+			AbstractDungeon.actionManager.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+			
+			AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(POTIONTHROWNEXTRADAMAGE, true), DamageType.NORMAL, AbstractGameAction.AttackEffect.SMASH));
+			
+		}
+		
+		
 	}
 	
 }
